66e45f10094763244b9f7197169c7e6b7bd7b55a
Latifah Lillie <queen1892@gmail.com>
2021-03-06 22:35:01 -0600

    Fixing Issues with Deploying to Heroku

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
-// POST route creates
+
 router.post("/api/transaction", ({body}, res) => {
   Transaction.create(body)
     .then(dbTransaction => {
@@ -12,24 +12,24 @@ router.post("/api/transaction", ({body}, res) => {
     });
 });
 
-router.post("/api/transaction/bulk", ({body}, res) => {
-    Transaction.insertMany(body)
-      .then(dbTransaction => {
-        res.json(dbTransaction);
-      })
-      .catch(err => {
-        res.status(404).json(err);
-      });
-  });
+// router.post("/api/transaction/bulk", ({body}, res) => {
+//     Transaction.insertMany(body)
+//       .then(dbTransaction => {
+//         res.json(dbTransaction);
+//       })
+//       .catch(err => {
+//         res.status(404).json(err);
+//       });
+//   });
 
-  router.get("/api/transaction", (req, res) => {
-    Transaction.find({}).sort({date: -1})
-      .then(dbTransaction => {
-        res.json(dbTransaction);
-      })
-      .catch(err => {
-        res.status(404).json(err);
-      });
-  });
+//   router.get("/api/transaction", (req, res) => {
+//     Transaction.find({}).sort({date: -1})
+//       .then(dbTransaction => {
+//         res.json(dbTransaction);
+//       })
+//       .catch(err => {
+//         res.status(404).json(err);
+//       });
+//   });
 
 module.exports = router;
