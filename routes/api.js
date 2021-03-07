10c86cabea3209f4316b1cd821fa7fa74bfaf5ed
Latifah Lillie <queen1892@gmail.com>
2021-03-06 22:39:41 -0600

    Adding the Get and Post Routes back for Heroku

--- routes/api.js
+++ routes/api.js
@@ -22,14 +22,14 @@ router.post("/api/transaction", ({body}, res) => {
 //       });
 //   });
 
-//   router.get("/api/transaction", (req, res) => {
-//     Transaction.find({}).sort({date: -1})
-//       .then(dbTransaction => {
-//         res.json(dbTransaction);
-//       })
-//       .catch(err => {
-//         res.status(404).json(err);
-//       });
-//   });
+  router.get("/api/transaction", (req, res) => {
+    Transaction.find({}).sort({date: -1})
+      .then(dbTransaction => {
+        res.json(dbTransaction);
+      })
+      .catch(err => {
+        res.status(404).json(err);
+      });
+  });
 
 module.exports = router;
